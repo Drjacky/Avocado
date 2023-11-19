9e9730e35448bb3972b15d855b9ce0d896611c76
Drjacky <dr.jacky.2005@gmail.com>
2023-11-19 13:25:15 +0000

    Update right click action class

--- src/main/kotlin/com/github/drjacky/avocado/actions/RightClickAction.kt
+++ src/main/kotlin/com/github/drjacky/avocado/actions/RightClickAction.kt
@@ -1,14 +1,8 @@
-package com.github.drjacky.avocado.actions
-
-import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.json.JsonLanguage
-import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiFile
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
@@ -26,43 +20,48 @@ class RightClickAction : AnAction() {
 
         // Continue with the script execution
         val selectedFile = event.getData(CommonDataKeys.VIRTUAL_FILE)
-        if (selectedFile != null && isXmlFileInDrawableFolder(selectedFile)) {
+        if (selectedFile != null) {
             val fullPath = selectedFile.path
-            try {
-                // Build the command to execute
-                val command = arrayOf(NODE_EXECUTABLE, AVOCADO_EXECUTABLE_PATH, fullPath)
+            if (isXmlFileInDrawableFolder(selectedFile)) {
+                try {
+                    // Build the command to execute
+                    val command = arrayOf(NODE_EXECUTABLE, AVOCADO_EXECUTABLE_PATH, fullPath)
 
-                // Set the working directory (optional)
-                val workingDirectory = selectedFile.parent?.path
-                val processBuilder = ProcessBuilder(*command)
-                if (workingDirectory != null) {
-                    processBuilder.directory(File(workingDirectory))
+                    // Set the working directory (optional)
+                    val workingDirectory = selectedFile.parent?.path
+                    val processBuilder = ProcessBuilder(*command)
+                    if (workingDirectory != null) {
+                        processBuilder.directory(File(workingDirectory))
+                    }
+
+                    // Start the process
+                    val process = processBuilder.start()
+
+                    // Read the output (if any)
+                    val reader = BufferedReader(InputStreamReader(process.inputStream))
+                    var line: String?
+                    val outputStringBuilder = StringBuilder()
+                    while (reader.readLine().also { line = it } != null) {
+                        println(line)
+                        outputStringBuilder.append(line).append("\n")
+                    }
+
+                    // Wait for the process to complete
+                    val exitCode = process.waitFor()
+                    println("Script executed with exit code: $exitCode")
+
+                    // Handle the output as needed
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
                 }
-
-                // Start the process
-                val process = processBuilder.start()
-
-                // Read the output (if any)
-                val reader = BufferedReader(InputStreamReader(process.inputStream))
-                var line: String?
-                val outputStringBuilder = StringBuilder()
-                while (reader.readLine().also { line = it } != null) {
-                    println(line)
-                    outputStringBuilder.append(line).append("\n")
-                }
-
-                // Wait for the process to complete
-                val exitCode = process.waitFor()
-                println("Script executed with exit code: $exitCode")
-
-                // Handle the output as needed
-
-            } catch (e: Exception) {
-                e.printStackTrace()
+            } else {
+                // Handle the case where selectedFile is not in the expected folder
+                Messages.showErrorDialog("Selected file is not in the expected folder.", "Error")
             }
         } else {
-            // Handle the case where selectedFile is null or not in the expected folder
-            Messages.showErrorDialog("Invalid file or folder.", "Error")
+            // Handle the case where selectedFile is null
+            Messages.showErrorDialog("Selected file is null.", "Error")
         }
     }
 
@@ -79,11 +78,15 @@ class RightClickAction : AnAction() {
 
     private fun installNode() {
         // You can add platform-specific installation instructions here
-        Messages.showMessageDialog("Node.js is not installed. Please install Node.js.", "Node.js Not Found", Messages.getErrorIcon())
+        Messages.showMessageDialog(
+            "Node.js is not installed. Please install Node.js.",
+            "Node.js Not Found",
+            Messages.getErrorIcon()
+        )
     }
 
     private fun isXmlFileInDrawableFolder(file: VirtualFile): Boolean {
-        return file.extension == "xml" && file.parent?.name == "drawable"
+        return file.extension == "xml" && "drawable" == file.parent?.name
     }
 
     override fun update(event: AnActionEvent) {
