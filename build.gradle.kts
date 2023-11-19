68b2b10bad6e252db6cbef939c0d0ded4bc40b65
Drjacky <dr.jacky.2005@gmail.com>
2023-11-19 13:13:48 +0000

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,9 @@ plugins {
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.3.0"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.18.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.3"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
 group = properties("pluginGroup")
@@ -28,7 +28,7 @@ repositories {
     jcenter()
 }
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.18.1")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.3")
 }
 
 // Configure gradle-intellij-plugin plugin.
