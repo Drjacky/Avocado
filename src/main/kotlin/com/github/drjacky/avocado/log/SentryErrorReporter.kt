50b85b80aafe219f81a3007684b71d1b000ca1ef
Drjacky <dr.jacky.2005@gmail.com>
2024-05-19 17:40:29 +0100

    Remove comments

--- src/main/kotlin/com/github/drjacky/avocado/log/SentryErrorReporter.kt
+++ src/main/kotlin/com/github/drjacky/avocado/log/SentryErrorReporter.kt
@@ -64,18 +64,4 @@ class SentryErrorReporter : ErrorReportSubmitter() {
         }.queue()
         return true
     }
-
-    /*override fun initComponent() {
-        val pluginVersion = System.getProperty("pluginVersion")
-
-        Sentry.init { options ->
-            options.dsn = "https://f72444f4b2395b4da3731fba282f7898@o4507282921816064.ingest.de.sentry.io/4507282932498512"
-            options.environment = "production"
-            options.tracesSampleRate = 1.0
-            options.release = pluginVersion
-            options.isDebug = true
-            options.shutdownTimeoutMillis = 5000
-            options.setDiagnosticLevel(SentryLevel.ERROR)
-        }
-    }*/
 }
